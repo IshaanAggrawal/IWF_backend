823c1031e12a01af889b8c3d36bc2dbc87fb18b6
IshaanAggrawal <ishaanaggrawal101@gmail.com>
2026-05-30 16:16:27 +0530

    added cloudinary

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import paymentRoutes from "./src/routes/payment.routes.js";
-import 'dotenv/config'; // Load environment variables
+import uploadRoutes from "./src/routes/upload.routes.js";
+import 'dotenv/config';
 
 const app = express();
 
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Your Express server is running.');
+    res.send('Your Express server is running.');
 });
 
 app.use('/api/payment', paymentRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
